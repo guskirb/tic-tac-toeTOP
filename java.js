f99e491c191ea7546c0386dacbd97d1872efbc1c
guskirb <anguskirby1998@gmail.com>
2024-04-10 22:25:35 +0100

    Add code for tic tac toe game

--- java.js
+++ java.js
@@ -1,6 +1,47 @@
 
+function createPlayer(name) {
+    const playerName = name;
+    const playerScore = ["", "", "", "", "", "", "", "", ""];
+    let turn = 0;
 
-const game = (function () {
-    const gameboard = { board: [a1, a2, a3, b1, b2, b3, c1, c2, c3] };
+    const nextTurn = () => turn++;
+    const getTurn = () => turn;
 
+    return { playerName, playerScore, nextTurn, getTurn };
+}
+
+const winConditions = (function () {
+
+    function checkWinner(player) {
+        const win = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let x = 0; x < win.length; x++) {
+            let winner = win[x]
+            let win1 = winner[0];
+            let win2 = winner[1];
+            let win3 = winner[2];
+            if (win1 === player[win1] && win2 === player[win2] && win3 === player[win3]) {
+                console.log("win");
+                declareWinner(player);
+                break;
+            }
+        }
+    };
+
+
+    return { checkWinner };
 })();
+
+
+
+
+
+
