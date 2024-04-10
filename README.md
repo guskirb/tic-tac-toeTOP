f073328cdc3f3b36d8b72bea661c4c7eda98602c
guskirb <anguskirby1998@gmail.com>
2024-04-10 19:30:15 +0100

    Add html, css and java files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tic_tac_toe-TOP
+
+I will be creating a Tic Tac Toe game to be played in the browser.
---
+++ java.js
@@ -0,0 +1,6 @@
+
+
+const game = (function () {
+    const gameboard = { board: [a1, a2, a3, b1, b2, b3, c1, c2, c3] };
+
+})();
