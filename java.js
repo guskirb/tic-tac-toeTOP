9112032d8b75a99aedce4e1c8a826f9f4a69e8cd
guskirb <anguskirby1998@gmail.com>
2024-04-10 23:24:18 +0100

    Fix code to make player score a number

--- java.js
+++ java.js
@@ -1,17 +1,34 @@
+const squares = document.querySelectorAll(".square");
+
+// player factory function
 
 function createPlayer(name) {
     const playerName = name;
-    const playerScore = ["", "", "", "", "", "", "", "", ""];
+    const score = ["", "", "", "", "", "", "", "", ""];
     let turn = 0;
 
     const nextTurn = () => turn++;
     const getTurn = () => turn;
 
-    return { playerName, playerScore, nextTurn, getTurn };
-}
+    return { playerName, score, nextTurn, getTurn };
+};
+
+const player1 = createPlayer("Gus");
+const player2 = createPlayer("PC");
+
+let currentPlayer = player1;
+let nextPlayer = player2;
+
+// win conditions IIFE module
 
 const winConditions = (function () {
-
+    function checkScore(play1, play2) {
+        if (play1.getTurn() === 2) {
+            winConditions.checkWinner(play1.score);
+        } else if (play2.getTurn() === 2) {
+            winConditions.checkWinner(play2.score);
+        }
+    };
     function checkWinner(player) {
         const win = [
             [0, 1, 2],
@@ -28,20 +45,30 @@ const winConditions = (function () {
             let win1 = winner[0];
             let win2 = winner[1];
             let win3 = winner[2];
-            if (win1 === player[win1] && win2 === player[win2] && win3 === player[win3]) {
+            if (win1 === Number(player[win1]) && win2 === Number(player[win2]) && win3 === Number(player[win3])) {
                 console.log("win");
-                declareWinner(player);
-                break;
+
             }
         }
     };
-
-
-    return { checkWinner };
+    return { checkWinner, checkScore };
 })();
 
-
-
-
+squares.forEach(function (square) {
+    square.addEventListener("click", function play() {
+        currentPlayer.score[square.id] = square.id;
+        winConditions.checkScore(player1, player2);
+        if (currentPlayer === player1) {
+            player1.nextTurn();
+            currentPlayer = player2;
+            nextPlayer = player1
+        } else {
+            player2.nextTurn();
+            currentPlayer = player1;
+            nextPlayer = player2;
+        }
+        square.removeEventListener("click", play);
+    })
+});
 
 
